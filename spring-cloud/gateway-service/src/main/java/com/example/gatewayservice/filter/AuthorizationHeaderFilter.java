2d6e74bdffb09354768818da36f3c53e7226e82b
KimSeHwan <ksh9241@naver.com>
2024-03-31 22:35:37 +0900

    Spring Config Service

--- spring-cloud/gateway-service/src/main/java/com/example/gatewayservice/filter/AuthorizationHeaderFilter.java
+++ spring-cloud/gateway-service/src/main/java/com/example/gatewayservice/filter/AuthorizationHeaderFilter.java
@@ -1,2 +1,78 @@
-package com.example.gatewayservice.filter;public class AuthorizationHeaderFilter {
-}
+//package com.example.gatewayservice.filter;
+//
+//import io.jsonwebtoken.Jwts;
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.http.HttpHeaders;
+//import org.springframework.core.env.Environment;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.server.ServerHttpRequest;
+//import org.springframework.http.server.ServerHttpResponse;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//
+//@Component
+//@Slf4j
+//public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+//
+//    Environment env;
+//
+//    public AuthorizationHeaderFilter(Environment env) {
+//        this.env = env;
+//    }
+//
+//    public static class Config {
+//
+//    }
+//
+//    // login -> token -> users (with token) -> header(include token)
+//    public GatewayFilter apply(Config config) {
+//        return ((exchange, chain) -> {
+//            ServerHttpRequest req = exchange.getRequest();
+//
+//            if (!req.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+//                return onError(exchange, "No Authorization Header", HttpStatus.UNAUTHORIZED);
+//            }
+//
+//            String authorizationHeader = req.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+//            String jwt = authorizationHeader.replace("Bearer", "");
+//
+//            if (!isJwtValid(jwt)) {
+//                return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
+//            }
+//
+//
+//            return chain.filter(exchange);
+//        }) ;
+//    }
+//
+//    private boolean isJwtValid(String jwt) {
+//        boolean returnValue = true;
+//
+//        String subject = null;
+//
+//        try {
+//            subject = Jwts.parser().setSigningKey(env.getProperty("token.secret"))
+//                    .parseClaimsJws(jwt).getBody()
+//                    .getSubject();
+//        }
+//        catch (Exception ex) {
+//            returnValue = false;
+//        }
+//
+//        if (subject == null || subject.isEmpty()) {
+//            returnValue = false;
+//        }
+//
+//        return returnValue;
+//    }
+//
+//    // Mono, Flux -> String WebFlux
+//    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+//        ServerHttpResponse res = exchange.getResponse();
+//        res.setStatusCode(httpStatus);
+//
+//        return res.setComplete();
+//    }
+//
+//}
--- spring-cloud/gateway-service/src/main/java/com/example/gatewayservice/filter/CustomFilter.java
+++ spring-cloud/gateway-service/src/main/java/com/example/gatewayservice/filter/CustomFilter.java
@@ -1,39 +1,39 @@
-package com.example.gatewayservice.filter;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
-
-@Component
-@Slf4j
-/**
- * Custom Filter는 application.yml 파일에 라우터 별 Filter를 적용해줘야 한다.
- * */
-public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
-    public CustomFilter() {
-        super(Config.class);
-    }
-
-    @Override
-    public GatewayFilter apply(Config config) {
-        // Custom Pre Filter
-        return (exchange, chain) -> {
-            ServerHttpRequest req = exchange.getRequest();
-            ServerHttpResponse res = exchange.getResponse();
-
-            log.info("Custom PRE filter : request id -> {}", req.getId());
-
-            // Custom Post Filter
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                log.info("Custom POST filter : response code -> {}", res.getStatusCode());
-            }));
-        };
-    }
-
-
-    public static class Config {
-        // Put the Configuration properties
-    }
-}
+//package com.example.gatewayservice.filter;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.server.reactive.ServerHttpRequest;
+//import org.springframework.http.server.reactive.ServerHttpResponse;
+//import org.springframework.stereotype.Component;
+//import reactor.core.publisher.Mono;
+//
+//@Component
+//@Slf4j
+///**
+// * Custom Filter는 application.yml 파일에 라우터 별 Filter를 적용해줘야 한다.
+// * */
+//public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+//    public CustomFilter() {
+//        super(Config.class);
+//    }
+//
+//    @Override
+//    public GatewayFilter apply(Config config) {
+//        // Custom Pre Filter
+//        return (exchange, chain) -> {
+//            ServerHttpRequest req = exchange.getRequest();
+//            ServerHttpResponse res = exchange.getResponse();
+//
+//            log.info("Custom PRE filter : request id -> {}", req.getId());
+//
+//            // Custom Post Filter
+//            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//                log.info("Custom POST filter : response code -> {}", res.getStatusCode());
+//            }));
+//        };
+//    }
+//
+//
+//    public static class Config {
+//        // Put the Configuration properties
+//    }
+//}
